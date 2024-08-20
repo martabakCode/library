6421867cb73b85d80482394cd4135c59bd6503a0
martbakCode <rajendranohan4@gmail.com>
2024-08-20 17:41:01 +0700

    Responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,11 @@
             <a href="{{ route('books.show', $book->id) }}">
             <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
 
-                    <img class="rounded-t-lg" src="{{ asset('storage/' . $book->book_cover) }}" alt="Book Image" />
+                    <img class="rounded-t-lg max-h-72 min-h-72 w-full object-cover" src="{{ asset('storage/' . $book->book_cover) }}" alt="Book Image" />
 
                 <div class="p-5">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $book->title }}</h5>
-                        <h6 class="mb-2 text-lg tracking-tight text-gray-900 dark:text-white">{{ $book->author }}</h6>
+                        <h5 class="mb-2 text-lg md:text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $book->title }}</h5>
+                        <h6 class="mb-2 text-sm md:text-md tracking-tight text-gray-900 dark:text-white">{{ $book->author }}</h6>
                 </div>
             </div>
             </a>
